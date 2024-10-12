681fc54ddb435866d3303363aeebc8095c79cca9
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2024-10-12 19:35:08 +0200

    feat: added presistent dev browser data

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   srcDir: "src",
   modules: ["@wxt-dev/module-svelte"],
   runner: {
+    chromiumArgs: ["--user-data-dir=./private/.dev-profile"],
     startUrls: ["https://fansly.com/live/zergo0_bot"],
   },
 });
