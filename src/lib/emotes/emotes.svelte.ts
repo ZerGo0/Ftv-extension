6d4b8a14234a46f4c777dcb24754d2d46ad32157
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2024-10-17 20:14:45 +0200

    refactor: use emote.nameLower for emote search as well

--- src/lib/emotes/emotes.svelte.ts
+++ src/lib/emotes/emotes.svelte.ts
@@ -1,7 +1,7 @@
 import type { Emote } from "../types";
 
 class EmoteStore {
-  // NOTE: this has to be a $state object(?),
+  // NOTE: this has to be a $state,
   // because we want to re-render the UI when the content of this array changes.
   // This makes it way easier compared to using a store :)
   emotes: Emote[] = $state([]);
@@ -12,7 +12,7 @@ class EmoteStore {
 
   search(term: string) {
     return this.emotes.filter((emote) =>
-      emote.name.toLocaleLowerCase().includes(term.toLocaleLowerCase())
+      emote.nameLower.includes(term.toLocaleLowerCase())
     );
   }
 }
